6e04ab26332c0b74db5267af1ebbce6ade216b93
William Boquist <mouless@gmail.com>
2019-02-27 14:14:24 +0100

    Påbörjat arbete med att bryta ut LOGIK från PRESENTATION

---
+++ Cards/InitializeDeck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Cards
+{
+    public class InitializeDeck
+    {
+        public void CreateDeck()
+        {
+            DeckOfCards = new List<Card>();
+            for (Card.CardSuit i = Card.CardSuit.Hjärter; i <= Card.CardSuit.Klöver; i++)
+            {
+                for (Card.CardRank j = Card.CardRank.Två; j <= Card.CardRank.Ess; j++)
+                {
+                    DeckOfCards.Add(new Card(i, j));
+                }
+            }
+        }
+    }
+}
